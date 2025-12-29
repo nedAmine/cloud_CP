3864ccadc8c32de1b28af20db6d3252621005893
Hmidi <mohaned_amin@yahoo.fr>
2025-12-29 05:30:55 +0100

    done5

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use("/api/products", productRoutes);
 
 //SERVE THE FRONTEND built
 app.use(express.static(path.join(__dirname, "public")));
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
